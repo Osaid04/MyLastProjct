3fa077a5a37210d261f9598f64acf71b7d067121
Osaid04 <osaidhamayel04@gmail.com>
2024-08-22 09:42:46 +0300

    MediaQuery

--- script.js
+++ script.js
@@ -230,4 +230,9 @@ function searchon() {
         }
     }
 }
+//  toggle button to open left menu 
+document.querySelector('.toggle-btn').addEventListener('click', function() {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.classList.toggle('open');
+});
 
